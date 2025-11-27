edf2c1db2cfaddbfcc3cc6763e9a5cb3579d551d
jmindeveloper <jmin.developer@gmail.com>
2025-11-27 12:31:24 +0900

    Example app 생성

---
+++ Example/EventLoggerExample/EventLoggerExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventLoggerExample
+//
+//  Created by 김지민 on 11/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
