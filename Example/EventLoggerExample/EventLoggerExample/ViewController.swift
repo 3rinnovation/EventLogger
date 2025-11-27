feda427b306e57733b5031ad002a93d34e09cf3c
jmindeveloper <jmin.developer@gmail.com>
2025-11-27 14:31:17 +0900

    example 앱 개발

--- Example/EventLoggerExample/EventLoggerExample/ViewController.swift
+++ Example/EventLoggerExample/EventLoggerExample/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import EventLogger
 
 class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func touchLogButtonAction(_ sender: Any) {
+        guard let touchLogVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "TouchLogViewController") as? TouchLogViewController else {
+            return
+        }
+        
+        if let data = FingerTouchEvent.loadFromFile(type: .direct),
+           let logStr = String(data: data, encoding: .utf8) {
+            touchLogVC.logText = logStr
+        }
+
+        present(touchLogVC, animated: true)
+    }
+}
+
+class TouchLogViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    var logText: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textView.text = logText
     }
-
-
 }
-
